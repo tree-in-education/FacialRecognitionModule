6e5adedcb75e49d491c69b6dbb43cb88540600b6
t-williamss <33206136+t-williamss@users.noreply.github.com>
2020-04-23 17:46:56 -0700

    Update fac-mapping.py

--- fac-mapping.py
+++ fac-mapping.py
@@ -1,40 +1,47 @@
 from PIL import Image, ImageDraw
-import face_recognition
+import requests
+import json
 
-image_file = 'obama.jpg' 
+image_file = 'sample_images/obama.jpg' 
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file(image_file)
+# call face recognition REST API
+url = 'http://127.0.0.1:5001/face_rec'
+files = {'file': open(image_file, 'rb')} 
+params = {'facial_features': 'true', 'face_locations':'true'}
+resp = requests.post(url, files = files, params = params)
 
-# Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+# get facial features
+resp_dict = resp.json() # convert to dict
+facial_features = resp_dict['facial_features']
+face_locations = resp_dict['face_locations']
 
-print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
-
-pil_image = Image.fromarray(image)
+pil_image = Image.open(image_file)
 d = ImageDraw.Draw(pil_image)
 
-for face_landmarks in face_landmarks_list:
-    # Print the location of each facial feature in this image
-    facial_features = [
-        'chin',
-        'left_eyebrow',
-        'right_eyebrow',
-        'nose_bridge',
-        'nose_tip',
-        'left_eye',
-        'right_eye',
-        'top_lip',
-        'bottom_lip'
-    ]
+facial_feature_names = [
+    'chin',
+    'left_eyebrow',
+    'right_eyebrow',
+    'nose_bridge',
+    'nose_tip',
+    'left_eye',
+    'right_eye',
+    'top_lip',
+    'bottom_lip'
+]
 
-    for facial_feature in facial_features:
-        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+def convert_list(list_of_list):
+    list_of_tuple = [tuple(i) for i in list_of_list]
+    return list_of_tuple
 
-    # Let's trace out each facial feature in the image with a line!
-    for facial_feature in facial_features:
-        d.line(face_landmarks[facial_feature], width=5)
+# Let's trace out each facial feature in the image with a line!
+for feature_name in facial_feature_names:
+    d.line( convert_list(facial_features[feature_name]), width=5)
+
+# draw rectangle for face location:
+top, right, bottom, left = face_locations
+d.rectangle([left,top,right,bottom],outline='red')
 
 # Display drawed image
 pil_image.show()
-#　pil_image.save('test.png')
+pil_image.save('face_makeup.png')
